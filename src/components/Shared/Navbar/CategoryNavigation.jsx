030768dc258fc60f1c8348058fc4142a014db355
Astro <rgrxmahabub.official@gmail.com>
2025-01-06 02:12:45 +0600

    navbar updated

--- src/components/Shared/Navbar/CategoryNavigation.jsx
+++ src/components/Shared/Navbar/CategoryNavigation.jsx
@@ -1,22 +1,52 @@
+"use client";
+
+import { useState } from "react";
 import { useGetAllCategoriesQuery } from "@/redux/services/category/categoryApi";
-import { DownOutlined, RightOutlined } from "@ant-design/icons";
-import { Menu, Dropdown } from "antd";
+import {
+  ArrowLeftOutlined,
+  MenuOutlined,
+  RightOutlined,
+} from "@ant-design/icons";
+import { Drawer, Button, Menu } from "antd";
 import Link from "next/link";
-import React from "react";
 
 const CategoryNavigation = () => {
   const { data: categories } = useGetAllCategoriesQuery();
+  const [drawerVisible, setDrawerVisible] = useState(false);
+  const [subCategoryDrawerVisible, setSubCategoryDrawerVisible] =
+    useState(false);
+  const [currentCategory, setCurrentCategory] = useState(null);
+
+  const openDrawer = () => setDrawerVisible(true);
+
+  const closeDrawer = () => {
+    setDrawerVisible(false);
+    setSubCategoryDrawerVisible(false);
+    setCurrentCategory(null);
+  };
+
+  const openSubCategoryDrawer = (category) => {
+    if (category?.categories?.length > 0) {
+      setCurrentCategory(category);
+      setSubCategoryDrawerVisible(true);
+    }
+  };
+
+  const backToParentDrawer = () => {
+    setSubCategoryDrawerVisible(false);
+    setCurrentCategory(null);
+  };
 
   const renderSubcategories = (category) => {
-    if (category?.subcategories && category?.subcategories.length > 0) {
+    if (category?.categories?.length > 0) {
       return (
         <Menu>
-          {category.subcategories.map((subCategory) => (
+          {category.categories.map((subCategory) => (
             <Menu.Item key={subCategory?._id}>
               <Link href={`/products?filter=${subCategory?.name}`}>
                 {subCategory?.name}
-                {subCategory?.subcategories &&
-                  subCategory?.subcategories.length > 0 && (
+                {subCategory?.categories &&
+                  subCategory?.categories.length > 0 && (
                     <RightOutlined className="ml-2" />
                   )}
               </Link>
@@ -24,64 +54,87 @@ const CategoryNavigation = () => {
           ))}
         </Menu>
       );
+    } else {
+      return <p>No categories available</p>;
     }
-    return null;
-  };
-
-  const renderCategories = (parentCategory) => {
-    return (
-      <Menu>
-        {parentCategory?.categories?.map((category) => (
-          <Menu.SubMenu
-            key={category?._id}
-            title={
-              <Link
-                href={`/products?filter=${category?.name}`}
-                className="flex items-center"
-              >
-                {category?.name}
-              </Link>
-            }
-          >
-            {renderSubcategories(category)}
-          </Menu.SubMenu>
-        ))}
-      </Menu>
-    );
   };
 
   const renderParentCategories = () => {
     return categories?.results
       ?.filter((item) => item?.level === "parentCategory")
-      .map((parentCategory) => (
-        <Dropdown
-          key={parentCategory?._id}
-          overlay={renderCategories(parentCategory)}
-          trigger={["hover"]}
-        >
-          <Link
-            href={`/products?filter=${parentCategory?.name}`}
-            className="flex items-center cursor-pointer"
+      .map((parentCategory) => {
+        return parentCategory?.categories?.length > 0 ? (
+          <Menu.Item
+            key={parentCategory?._id}
+            onClick={() => openSubCategoryDrawer(parentCategory)}
           >
-            <span>{parentCategory?.name}</span>
-            {parentCategory?.categories &&
-              parentCategory?.categories.length > 0 && (
-                <DownOutlined className="!text-sm" />
-              )}
-          </Link>
-        </Dropdown>
-      ));
+            <div className="flex items-center">
+              {parentCategory?.name}
+              {parentCategory?.categories &&
+                parentCategory?.categories.length > 0 && (
+                  <RightOutlined className="ml-2" />
+                )}
+            </div>
+          </Menu.Item>
+        ) : (
+          <Menu.Item key={parentCategory?._id}>
+            <Link
+              href={`/products?filter=${parentCategory?.name}`}
+              className="flex items-center"
+            >
+              {parentCategory?.name}
+              {parentCategory?.categories &&
+                parentCategory?.categories.length > 0 && (
+                  <RightOutlined className="ml-2" />
+                )}
+            </Link>
+          </Menu.Item>
+        );
+      });
   };
 
   return (
-    <div className="my-container">
-      <div className="flex flex-col lg:flex-row gap-5 lg:items-center flex-wrap justify-center py-5">
-        <Link href={"/offers"}>Offers</Link>
-        <Link href={"/products"}>All Products</Link>
-        <span className="hidden lg:block">|</span>
-        <span className="lg:hidden"></span>
-        {renderParentCategories()}
+    <div className="px-2 lg:px-5 -my-2">
+      <div className="flex items-center justify-between py-2 lg:py-3 text-white gap-5">
+        <div
+          className="flex items-center text-white hover:text-white gap-2"
+          onClick={openDrawer}
+        >
+          <MenuOutlined />
+          All
+        </div>
+        <div className="flex gap-5 flex-wrap py-3 text-white">
+          <Link href={"/offers"}>Offers</Link>
+          <Link href={"/products"}>All Products</Link>
+        </div>
       </div>
+
+      <Drawer
+        title="Categories"
+        placement="left"
+        onClose={closeDrawer}
+        open={drawerVisible}
+        width={300}
+      >
+        <div className="flex flex-col py-3 text-black">
+          <Menu>{renderParentCategories()}</Menu>
+        </div>
+      </Drawer>
+
+      <Drawer
+        placement="left"
+        onClose={closeDrawer}
+        open={subCategoryDrawerVisible}
+        width={300}
+      >
+        <div className="flex items-center">
+          <Button onClick={backToParentDrawer} icon={<ArrowLeftOutlined />} />
+          <span className="ml-2">{currentCategory?.name}</span>
+        </div>
+        <div className="flex flex-col py-3 text-black">
+          {currentCategory ? renderSubcategories(currentCategory) : null}
+        </div>
+      </Drawer>
     </div>
   );
 };
--- src/components/Shared/Navbar/LandingHeader.jsx
+++ src/components/Shared/Navbar/LandingHeader.jsx
@@ -1,34 +1,32 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 
+import { useGetSingleUserQuery } from "@/redux/services/auth/authApi";
+import { logout, useCurrentUser } from "@/redux/services/auth/authSlice";
+import { useGetSingleCartByUserQuery } from "@/redux/services/cart/cartApi";
+import { useGetSingleCompareByUserQuery } from "@/redux/services/compare/compareApi";
+import { useDeviceId } from "@/redux/services/device/deviceSlice";
 import { useGetAllGlobalSettingQuery } from "@/redux/services/globalSetting/globalSettingApi";
 import { useGetAllProductsQuery } from "@/redux/services/product/productApi";
+import { useGetSingleWishlistByUserQuery } from "@/redux/services/wishlist/wishlistApi";
 import { formatImagePath } from "@/utilities/lib/formatImagePath";
-import { MenuOutlined, UserOutlined } from "@ant-design/icons";
+import { UserOutlined } from "@ant-design/icons";
 import { AutoComplete, Avatar, Button, Drawer, Popover } from "antd";
 import Image from "next/image";
 import Link from "next/link";
-import { useEffect, useState } from "react";
-import { FaHeart, FaSearch, FaShoppingBag, FaUser } from "react-icons/fa";
-import { FaCodeCompare } from "react-icons/fa6";
-import CategoryNavigation from "./CategoryNavigation";
-import { useDispatch, useSelector } from "react-redux";
-import { logout, useCurrentUser } from "@/redux/services/auth/authSlice";
-import { useDeviceId } from "@/redux/services/device/deviceSlice";
-import { useGetSingleUserQuery } from "@/redux/services/auth/authApi";
-import { useGetSingleCompareByUserQuery } from "@/redux/services/compare/compareApi";
-import { useGetSingleWishlistByUserQuery } from "@/redux/services/wishlist/wishlistApi";
-import DrawerCart from "../Product/DrawerCart";
-import { GiCancel } from "react-icons/gi";
-import { useGetSingleCartByUserQuery } from "@/redux/services/cart/cartApi";
 import { usePathname } from "next/navigation";
-import { toast } from "sonner";
+import { useState } from "react";
+import { FaHeart, FaSearch, FaShoppingBag } from "react-icons/fa";
+import { FaCodeCompare } from "react-icons/fa6";
+import { GiCancel } from "react-icons/gi";
 import { IoMdArrowDropdown } from "react-icons/io";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "sonner";
+import DrawerCart from "../Product/DrawerCart";
+import CategoryNavigation from "./CategoryNavigation";
 
 const LandingHeader = () => {
   const pathname = usePathname();
-  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-  const [isSearchOpen, setIsSearchOpen] = useState(false);
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [options, setOptions] = useState([]);
   const dispatch = useDispatch();
@@ -46,13 +44,7 @@ const LandingHeader = () => {
   );
 
   const { data: globalData } = useGetAllGlobalSettingQuery();
-  const { data: products } = useGetAllProductsQuery(undefined, {
-    skip: !isSearchOpen,
-  });
-
-  const toggleDrawer = () => {
-    setIsDrawerOpen(!isDrawerOpen);
-  };
+  const { data: products } = useGetAllProductsQuery();
 
   const handleLogout = () => {
     dispatch(logout());
@@ -66,26 +58,6 @@ const LandingHeader = () => {
     Wishlist: `/${data?.role}/orders/wishlist`,
     Cart: `/${data?.role}/orders/cart`,
   };
-
-  const [showNavbar, setShowNavbar] = useState(true);
-  const [lastScrollY, setLastScrollY] = useState(0);
-
-  const handleScroll = () => {
-    const currentScrollY = window.scrollY;
-    if (currentScrollY > lastScrollY) {
-      setShowNavbar(false);
-    } else {
-      setShowNavbar(true);
-    }
-    setLastScrollY(currentScrollY);
-  };
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [lastScrollY]);
 
   const content = (
     <div>
@@ -167,35 +139,11 @@ const LandingHeader = () => {
 
   return (
     <header
-      className={`fixed top-0 left-0 w-full bg-white shadow-md transition-transform duration-300 z-50 ${
-        showNavbar ? "translate-y-0" : "-translate-y-full"
-      }`}
+      className={`w-full bg-[#0f1111] shadow-md transition-transform duration-300 z-50`}
     >
-      <div
-        className={`bg-primary py-2 text-center text-white px-2 text-sm md:text-base ${
-          lastScrollY > 0 ? "hidden" : ""
-        }`}
-      >
-        To order any product, please call or whatsapp us at{" "}
-        <span className="font-semibold">
-          {globalData?.results?.businessWhatsapp}
-        </span>
-      </div>
-      <nav className="my-container px-2 -my-5 lg:-my-2">
-        <div className="flex justify-around items-center">
-          <Button
-            type="text"
-            className="lg:hidden"
-            icon={<MenuOutlined />}
-            onClick={toggleDrawer}
-          />
-          <div>
-            <FaSearch
-              className="cursor-pointer hover:text-primary duration-300 hidden lg:block text-xl text-primary"
-              onClick={() => setIsSearchOpen(true)}
-            />
-          </div>
-          <Link href={"/"} className="flex flex-[1] lg:flex-none lg:ml-10">
+      <nav className="px-2 py-2 lg:-my-3">
+        <div className="flex justify-around items-center gap-5">
+          <Link href={"/"} className="">
             <Image
               src={globalData?.results?.logo}
               alt="logo"
@@ -203,12 +151,18 @@ const LandingHeader = () => {
               height={100}
             />
           </Link>
+          <div className="relative w-full">
+            <AutoComplete
+              options={options}
+              onSearch={handleSearch}
+              placeholder="Search for Products..."
+              size="large"
+              className="w-full"
+            />
+            <FaSearch className="absolute right-2 top-1/2 -translate-y-1/2 text-primary text-xl" />
+          </div>
 
           <div className="flex gap-6 items-center text-lg">
-            <FaSearch
-              className="cursor-pointer hover:text-primary duration-300 lg:hidden"
-              onClick={() => setIsSearchOpen(true)}
-            />
             {user?._id ? (
               <>
                 {" "}
@@ -236,140 +190,83 @@ const LandingHeader = () => {
               </>
             ) : (
               <>
-                <Link href={"/sign-in"}>
-                  <FaUser className="cursor-pointer hover:text-primary duration-300" />
+                <Link
+                  href={"/sign-in"}
+                  className="text-sm text-white flex flex-col w-[100px] hover:text-primary duration-300"
+                >
+                  <span>Hello, Sign In</span>
+                  <span className="font-semibold">Accounts</span>
                 </Link>
               </>
             )}
 
-            <Link href={"/compare"} className="hidden lg:flex">
+            <Link
+              href={"/compare"}
+              className="hidden lg:flex text-white text-sm cursor-pointer"
+            >
               {compareData?.[0]?.product?.length > 0 ? (
-                <span className="relative">
+                <span className="relative flex items-center gap-2 hover:text-primary duration-300">
                   <span className="absolute -top-2 -right-2 bg-primary text-white rounded-full w-4 h-4 flex items-center justify-center text-xs">
                     {compareData?.[0]?.product?.length}
                   </span>
-                  <FaCodeCompare className="cursor-pointer rotate-90 hover:text-primary duration-300" />
+                  <FaCodeCompare className="rotate-90" />
+                  <span>Compare</span>
                 </span>
               ) : (
-                <FaCodeCompare className="cursor-pointer rotate-90 hover:text-primary duration-300" />
+                <div className="flex gap-2 items-center hover:text-primary duration-300">
+                  <FaCodeCompare className="rotate-90" />
+                  Compare
+                </div>
               )}
             </Link>
-            <Link href={"/wishlist"} className="hidden lg:flex">
+            <Link
+              href={"/wishlist"}
+              className="hidden lg:flex text-sm text-white cursor-pointer"
+            >
               {wishListData?.length > 0 ? (
-                <span className="relative">
+                <span className="relative flex items-center gap-2 hover:text-primary duration-300">
                   <span className="absolute -top-2 -right-2 bg-primary text-white rounded-full w-4 h-4 flex items-center justify-center text-xs">
                     {wishListData?.length}
                   </span>
-                  <FaHeart className="cursor-pointer hover:text-primary duration-300" />
+                  <FaHeart />
+                  Wishlist
                 </span>
               ) : (
-                <FaHeart className="cursor-pointer hover:text-primary duration-300" />
+                <div className="flex items-center gap-2 hover:text-primary duration-300">
+                  <FaHeart />
+                  Wishlist
+                </div>
               )}
             </Link>
-            <div className="hidden lg:flex">
+            <div
+              className="hidden lg:flex cursor-pointer text-sm text-white"
+              onClick={() => setIsCartOpen(true)}
+            >
               {cartData?.length > 0 ? (
-                <span className="relative">
+                <span className="relative flex items-center gap-2 hover:text-primary duration-300">
                   <span className="absolute -top-2 -right-2 bg-primary text-white rounded-full w-4 h-4 flex items-center justify-center text-xs">
                     {cartData?.length}
                   </span>
-                  <FaShoppingBag
-                    className="cursor-pointer hover:text-primary duration-300"
-                    onClick={() => setIsCartOpen(true)}
-                  />
+                  <FaShoppingBag />
+                  Cart
                 </span>
               ) : (
-                <FaShoppingBag
-                  className="cursor-pointer hover:text-primary duration-300"
+                <div
+                  className="flex items-center gap-2 hover:text-primary duration-300"
                   onClick={() => setIsCartOpen(true)}
-                />
+                >
+                  <FaShoppingBag />
+                  Cart
+                </div>
               )}
             </div>
           </div>
         </div>
       </nav>
-      <div className="hidden lg:flex gap-6 items-center bg-grey">
+      <div className="flex gap-6 items-center bg-[#232f3e]">
         <CategoryNavigation />
       </div>
-      <Drawer
-        placement="left"
-        onClose={() => setIsDrawerOpen(false)}
-        open={isDrawerOpen}
-      >
-        <div className="flex justify-between items-center -mt-5 mb-5">
-          <Link href={"/"}>
-            <Image
-              src={globalData?.results?.logo}
-              alt="logo"
-              width={80}
-              height={80}
-            />
-          </Link>
-          <button
-            className="mt-1 bg-gray-200 hover:scale-110 duration-500 rounded-full p-1"
-            onClick={() => setIsDrawerOpen(false)}
-          >
-            <GiCancel className="text-xl text-gray-700" />
-          </button>
-        </div>
-        <CategoryNavigation />
-      </Drawer>
-      <Drawer
-        open={isSearchOpen}
-        onCancel={() => setIsSearchOpen(false)}
-        footer={null}
-        keyboard={true}
-        destroyOnClose
-        placement="top"
-        height={200}
-      >
-        <div className="flex justify-between items-center -mt-2">
-          <div></div>
-          <button
-            className="mt-1 bg-gray-200 hover:scale-110 duration-500 rounded-full p-1"
-            onClick={() => setIsSearchOpen(false)}
-          >
-            <GiCancel className="text-xl text-gray-700" />
-          </button>
-        </div>
-        <div className="p-5 flex items-center justify-between gap-10">
-          <Link href={"/"} className="hidden lg:flex">
-            <Image
-              src={globalData?.results?.logo}
-              alt="logo"
-              width={80}
-              height={80}
-            />
-          </Link>
-          <div className="relative w-full">
-            <AutoComplete
-              options={options}
-              onSearch={handleSearch}
-              placeholder="Search for Products..."
-              size="large"
-              className="w-full"
-            />
-            <FaSearch className="absolute right-8 top-1/2 -translate-y-1/2 text-primary text-xl" />
-          </div>
-          <div className="hidden lg:flex">
-            {cartData?.length > 0 ? (
-              <span className="relative">
-                <span className="absolute -top-2 -right-2 bg-primary text-white rounded-full w-4 h-4 flex items-center justify-center text-xs">
-                  {cartData?.length}
-                </span>
-                <FaShoppingBag
-                  className="cursor-pointer hover:text-primary duration-300 text-2xl"
-                  onClick={() => setIsCartOpen(true)}
-                />
-              </span>
-            ) : (
-              <FaShoppingBag
-                className="cursor-pointer hover:text-primary duration-300 text-2xl"
-                onClick={() => setIsCartOpen(true)}
-              />
-            )}
-          </div>
-        </div>
-      </Drawer>
+
       <Drawer
         placement="right"
         onClose={() => setIsCartOpen(false)}
